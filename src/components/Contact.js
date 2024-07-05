4a3c648b405f66e351114d31e27f305182427d33
dev-montgomery <montgomery.richard.dev@gmail.com>
2024-07-05 00:16:22 -0700

    working on page content in story.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div>
-      
+    <div className='contact-container'>
+      <h2></h2>
+      <p></p>
     </div>
   )
 };
--- src/components/Story.js
+++ src/components/Story.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Story = () => {
   return (
-    <div>
-      
+    <div className='story-container'>
+      <h2>About Sproutlings</h2>
+      <p>Our story begins, more or less, with a global pandemic. Just one global pandemic ago.</p>
+      <p>A lot of people faced some life changing events during that time, and for us, we started a family. We quickly began to look for whatever silver linings a novel virus spiking globally could offer us. I don't mean to make light of the time, but the uncertainty kind of left us in a new space to decide what to do with.</p>
+      <p>First, we quit our jobs for the safety of our unborn; and while I took the opportunity to learn web development, Jaci pursued a certification in medical coding. Suddenly we had a baby, and it turns out things would become harder to do then. Ultimately I was fortunate to become a full-time dad, and Jaci found employment with pediatrician Dr. Shaw in Silverdale. Shaw treated four generations of patients. Jaci found records from when Shaw treated my sister 35 years ago. Ok, that aside, I never managed to put together a web development portfolio.</p>
+      <p>Now that our little guy is no longer eating the corners of books, climbing furniture with a devil may care attitude, and is a little more aware of corners, I feel I've slowly been recovering the time to create something. Web development feels a little more out of reach with the advancement of AI. You suddenly almost don't need to know how to code. You just need to know how to ask.</p>
+      <p>Consequently I've been looking for other opportunities. Eventually I came across microgreens as a startup, and knew it was something within our capabilities. Jaci has homemaker skills. She knits, crochets, bakes, cooks, we still have sourdough starter from the early days of the pandemic that she maintains. She also likes to garden. This year she's constructed her own little space of zen, a greenhouse. That being said, microgreens was opening a door I didn't know was there.</p>
+      <p>I started to learn about</p>
     </div>
   )
 };
